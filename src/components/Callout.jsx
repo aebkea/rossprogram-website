b2152f934945d504df44ad3f291d6d7435edebb9
Abel Aebker <abel@qetyrop.com>
2023-06-13 09:43:11 -0400

    restyle callouts

--- src/components/Callout.jsx
+++ src/components/Callout.jsx
@@ -6,12 +6,16 @@ const styles = {
   note: {
     container:
       'bg-sky-50 dark:bg-slate-800/60 dark:ring-1 dark:ring-slate-300/10',
+    line:
+      'bg-sky-200 dark:bg-sky-800',
     title: 'text-sky-900 dark:text-sky-400',
     body: 'text-sky-800 [--tw-prose-background:theme(colors.sky.50)] prose-a:text-sky-900 prose-code:text-sky-900 dark:text-slate-300 dark:prose-code:text-slate-300',
   },
   warning: {
     container:
       'bg-amber-50 dark:bg-slate-800/60 dark:ring-1 dark:ring-slate-300/10',
+    line:
+      'bg-amber-200 dark:bg-amber-800',
     title: 'text-amber-900 dark:text-amber-500',
     body: 'text-amber-800 [--tw-prose-underline:theme(colors.amber.400)] [--tw-prose-background:theme(colors.amber.50)] prose-a:text-amber-900 prose-code:text-amber-900 dark:text-slate-300 dark:[--tw-prose-underline:theme(colors.sky.700)] dark:prose-code:text-slate-300',
   },
@@ -26,8 +30,10 @@ export function Callout({ type = 'note', title, children }) {
   let IconComponent = icons[type]
 
   return (
-    <div className={clsx('my-8 flex rounded-3xl p-6', styles[type].container)}>
-      {/* <IconComponent className="h-8 w-8 flex-none" /> */}
+    <div className={clsx('relative my-8 p-4 flex', styles[type].container)}>
+      <IconComponent className="h-8 w-8 flex-none" />
+      <span className={clsx('absolute inset-y-0 left-0 w-1', styles[type].line)} />
+      <span className={clsx('absolute inset-y-0 right-0 w-1', styles[type].line)} />
       <div className="ml-4 flex-auto">
         <p className={clsx('m-0 font-display text-xl', styles[type].title)}>
           {title}
