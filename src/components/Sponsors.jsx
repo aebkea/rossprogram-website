33dc7256db9e0d83035310f31da73a8cb39cbbda
Abel Aebker <abel@qetyrop.com>
2023-09-26 15:05:22 -0400

    make whole sponsor button clickable

--- src/components/Sponsors.jsx
+++ src/components/Sponsors.jsx
@@ -14,20 +14,17 @@ export function Sponsors({ columns, children }) {
 
 export function Sponsor({ title, description, href, logo }) {
     return (
-        <div className="group relative grid content-center rounded-xl border border-slate-200 dark:border-slate-800">
+        <Link href={href} className="group relative grid content-center rounded-xl border border-slate-200 dark:border-slate-800">
             <div className="absolute -inset-px rounded-xl border-2 border-transparent opacity-0 [background:linear-gradient(var(--quick-links-hover-bg,theme(colors.violet.50)),var(--quick-links-hover-bg,theme(colors.violet.50)))_padding-box,linear-gradient(to_top,theme(colors.indigo.400),theme(colors.cyan.400),theme(colors.violet.500))_border-box] group-hover:opacity-100 dark:[--quick-links-hover-bg:theme(colors.slate.800)]" />
             <div className="relative overflow-hidden rounded-xl p-6">
                 <SponsorLogo logo={logo} className={clsx("mx-auto", logo === "cakeShopCapital" ? "w-2/3" : "w-full")} />
                 <h2 className={clsx("font-display text-base text-slate-900 dark:text-white", title === undefined ? "" : "mt-4")}>
-                    <Link href={href}>
-                        <span className="absolute -inset-px rounded-xl" />
-                        {title}
-                    </Link>
+                    {title}
                 </h2>
                 <p className={clsx("text-sm text-slate-700 dark:text-slate-400", description === undefined ? "" : "mt-1")}>
                     {description}
                 </p>
             </div>
-        </div>
+        </Link>
     )
 }
