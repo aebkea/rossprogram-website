026520fc9464d3902ecbf8364e113c3896c6955b
Abel Aebker <abel@qetyrop.com>
2023-12-25 13:44:50 -0500

    add back not-prose

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -18,7 +18,7 @@ export default function NotFound() {
         <body class="h-full">
         ```
       */}
-      <div className="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
+      <div className="not-prose grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
         <div className="text-center">
           <p className="text-base font-semibold text-violet-600">404</p>
           <h1 className="mt-4 text-3xl font-bold tracking-tight text-slate-900 sm:text-5xl dark:text-white">Page not found</h1>
