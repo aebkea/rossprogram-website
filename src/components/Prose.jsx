888ad9e14a4703a50d6f465a32e3f0051c100be6
Abel Aebker <abel@qetyrop.com>
2023-12-27 11:24:34 -0500

    tweak prose styles

--- src/components/Prose.jsx
+++ src/components/Prose.jsx
@@ -7,13 +7,13 @@ export function Prose({ as: Component = 'div', className, ...props }) {
         className,
         'prose prose-slate max-w-none dark:prose-invert dark:text-slate-400',
         // headings
-        'prose-headings:scroll-mt-28 prose-headings:font-display prose-headings:font-medium lg:prose-headings:scroll-mt-[8.5rem]',
+        'prose-headings:scroll-mt-28 prose-headings:font-display prose-headings:font-medium lg:prose-headings:scroll-mt-[8.5rem] prose-h1:text-3xl prose-headings:tracking-tight',
         // lead
         'prose-lead:text-slate-500 dark:prose-lead:text-slate-400',
         // links
         'prose-a:font-semibold dark:prose-a:text-violet-400',
         // link underline
-        'prose-a:no-underline prose-a:shadow-[inset_0_-2px_0_0_var(--tw-prose-background,#fff),inset_0_calc(-1*(var(--tw-prose-underline-size,4px)+2px))_0_0_var(--tw-prose-underline,theme(colors.violet.300))] hover:prose-a:[--tw-prose-underline-size:6px] dark:[--tw-prose-background:theme(colors.slate.900)] dark:prose-a:shadow-[inset_0_calc(-1*var(--tw-prose-underline-size,2px))_0_0_var(--tw-prose-underline,theme(colors.violet.800))] dark:hover:prose-a:[--tw-prose-underline-size:6px]',
+        'prose-a:no-underline prose-a:shadow-[inset_0_-3px_0_0_var(--tw-prose-background,#fff),inset_0_calc(-1*(var(--tw-prose-underline-size,4px)+2px))_0_0_var(--tw-prose-underline,theme(colors.violet.300))] hover:prose-a:[--tw-prose-underline-size:5px] dark:[--tw-prose-background:theme(colors.slate.900)] dark:prose-a:shadow-[inset_0_calc(-1*var(--tw-prose-underline-size,3px))_0_0_var(--tw-prose-underline,theme(colors.violet.800))] dark:hover:prose-a:[--tw-prose-underline-size:5px]',
         // pre
         'prose-pre:rounded-xl prose-pre:bg-slate-900 prose-pre:shadow-lg dark:prose-pre:bg-slate-800/60 dark:prose-pre:shadow-none dark:prose-pre:ring-1 dark:prose-pre:ring-slate-300/10',
         // hr
