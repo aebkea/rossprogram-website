c2e856491580e07f645f2b31769d2969fe8b099d
Abel Aebker <abel@qetyrop.com>
2023-12-31 03:03:01 -0500

    simplify ImageModal

--- src/components/ImageModal.jsx
+++ src/components/ImageModal.jsx
@@ -1,44 +1,8 @@
-import { useState, useEffect, Fragment } from 'react';
+import { Fragment } from 'react';
 import { Dialog, Transition } from '@headlessui/react';
-import { useWindowSize } from '@/lib/utils';
 import Image from 'next/image';
 
 export default function ImageModal({ open, setOpen, image, unoptimized, width, height, placeholder }) {
-  const [containerWidth, setContainerWidth] = useState(null);
-  const [needsScaling, setNeedsScaling] = useState(false)
-
-  const windowSize = useWindowSize()
-
-  useEffect(() => {
-    const updateHeight = () => {
-      if (image) {
-        const maxWidth = 672
-        const actualWidth = Math.min(maxWidth, windowSize.width)
-        const imageHeight = image?.height ?? height
-        const imageWidth = image?.width ?? width
-        const defaultHeight = actualWidth * imageHeight / imageWidth
-        if (defaultHeight >= 0.8 * windowSize.height) {
-          const scalingFactor = (0.8 * windowSize.height) / defaultHeight
-          const newWidth = actualWidth * scalingFactor
-          setContainerWidth(`${newWidth}px`)
-          setNeedsScaling(true)
-        } else {
-          setNeedsScaling(false)
-          setContainerWidth(null)
-        }
-      }
-    };
-
-    if (open) {
-      window.addEventListener('resize', updateHeight);
-      updateHeight();
-    }
-
-    return () => {
-      window.removeEventListener('resize', updateHeight);
-    };
-  }, [open, image, windowSize]);
-
   return (
     <Transition.Root show={open} as={Fragment}>
       <Dialog as="div" className="relative z-[60]" onClose={setOpen}>
@@ -65,7 +29,7 @@ export default function ImageModal({ open, setOpen, image, unoptimized, width, h
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-2xl sm:p-6 dark:bg-slate-900" style={ needsScaling ? { width: containerWidth } : {} }>
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-2xl sm:p-6 dark:bg-slate-900">
                 <Image id="imageModal" src={image} alt="" placeholder={placeholder} width={width} height={height} unoptimized={unoptimized} className="rounded-sm max-h-[calc(80vh-6rem)] object-contain" />
                 <div className="mt-5 sm:mt-6">
                   <button
