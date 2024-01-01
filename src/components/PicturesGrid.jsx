e4f04f2db8f3bb587278f40f2e2099f0c82ed883
Abel Aebker <abel@qetyrop.com>
2024-01-01 12:02:07 -0500

    c_fill with gravity

--- src/components/PicturesGrid.jsx
+++ src/components/PicturesGrid.jsx
@@ -14,7 +14,7 @@ function ImageItem({ image, openImageModal }) {
         <div className="relative grow w-full aspect-[3/2] rounded-md sm:rounded-xl overflow-hidden">
           <div className="absolute inset-0 bg-gradient-to-tr from-slate-300/20 via-slate-200/20 to-slate-300/20 animate-pulse" />
           <div className={clsx("absolute inset-0 transition-opacity duration-300", isLoaded ? "opacity-100" : "opacity-0")}>
-            <CldImage src={image.publicId} width="512" quality="auto" format="auto" alt="" className="object-cover" fill={true} onLoad={() => setIsLoaded(true)} />
+            <CldImage src={image.publicId} transformation="c_fill,g_auto,w_512,ar_3:2" quality="auto" format="auto" alt="" className="object-cover" fill={true} onLoad={() => setIsLoaded(true)} />
           </div>
         </div>
       </div>
