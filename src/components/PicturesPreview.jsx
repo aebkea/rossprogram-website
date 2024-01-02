9bac8202150a53bbb9894aa380aab4efe677ec48
Abel Aebker <abel@qetyrop.com>
2024-01-02 01:02:23 -0500

    make image preview a link

--- src/components/PicturesPreview.jsx
+++ src/components/PicturesPreview.jsx
@@ -1,6 +1,9 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { CldImage } from "@/components/CldImage";
+import { useWindowSize } from "@/lib/utils";
+
+import Link from "next/link";
 
 import clsx from "clsx";
 
@@ -8,8 +11,8 @@ function ImageItem({ image }) {
   const [isLoaded, setIsLoaded] = useState(false);
 
   return (
-    <div className="flex flex-col h-fit transition-all hover:scale-110 duration-300 delay-50 peer-hover:scale-50">
-      <div className="relative grow w-full aspect-[3/2] rounded-[2rem] ring-8 ring-white overflow-hidden">
+    <div className="flex flex-col h-fit">
+      <div className="relative grow w-full aspect-[3/2] rounded-[2rem] overflow-hidden ring-8 ring-white transition-all group-hover:ring-violet-200 duration-300">
         <div className="absolute inset-0 bg-gradient-to-tr from-slate-300/20 via-slate-200/20 to-slate-300/20 animate-pulse" />
         <div className={clsx("absolute inset-0 transition-opacity duration-300", isLoaded ? "opacity-100" : "opacity-0")}>
           <CldImage src={image.publicId} transformation="c_fill,g_auto,w_512,ar_3:2" quality="auto" format="auto" alt="" className="object-cover" fill={true} onLoad={() => setIsLoaded(true)} />
@@ -19,21 +22,35 @@ function ImageItem({ image }) {
   );
 }
 
-export function PicturesPreview({ images }) {
+export function PicturesPreview({ images, href }) {
 
-  const reducedImages = images.slice(0,6)
+  const windowSize = useWindowSize()
+  const [reducedImages, setReducedImages] = useState([images.slice(0,6)])
+  const [smallScreen, setSmallScreen] = useState(false)
+
+  useEffect(() => {
+    if (windowSize.width < 700) {
+      setReducedImages(images.slice(0,4))
+      setSmallScreen(true)
+    } else {
+      setReducedImages(images.slice(0,6))
+      setSmallScreen(false)
+    }
+  }, [windowSize, images])
 
   return (
-    <>
-      <div className="not-prose group flex justify-start w-full h-[calc(14rem*2/3)] rounded-[2rem] pr-0 lg:pr-4 xl:pr-0 cursor-pointer">
-        {images && reducedImages.map(image => (
-          <div className="relative w-16 transition-all group-hover:w-[calc((100%-14rem)/5)] last-of-type:group-hover:w-56 duration-300 last-of-type:w-56 flex-shrink last-of-type:flex-none">
-            <div className="absolute left-0 w-56">
-              <ImageItem key={image.assetId} image={image} />
+    <div className="not-prose">
+      <Link href={href}>
+        <div className="group flex justify-start w-full h-[calc(14rem*2/3)] rounded-[2rem] pr-0 lg:pr-4 xl:pr-0 cursor-pointer">
+          {images && reducedImages.map(image => (
+            <div className={clsx("relative w-16 transition-all last-of-type:group-hover:w-56 duration-300 last-of-type:w-56 flex-shrink last-of-type:flex-none", smallScreen ? "w-[calc((100%-14rem)/3)]" : "group-hover:w-[calc((100%-14rem)/5)]")}>
+              <div className="absolute left-0 w-56">
+                <ImageItem key={image.assetId} image={image} />
+              </div>
             </div>
-          </div>
-        ))}
-      </div>
-    </>
+          ))}
+        </div>
+      </Link>
+    </div>
   );
 }
--- src/pages/pictures/index.md
+++ src/pages/pictures/index.md
@@ -7,10 +7,6 @@ Here is a collection of pictures from years-past at the Ross Program. Please enj
 
 {% pictures-page expression="folder=\"\"" count="6" /%}
 
-## [2023](/pictures/2023)
+## 2023
 
-{% pictures-page expression="folder=2023" count="12" /%}
-
-## Test
-
-{% pictures-preview expression="folder=2023" /%}
+{% pictures-preview expression="folder=2023" href="/pictures/2023" /%}
